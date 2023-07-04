278c270d51054d1f3ccf3677bd81093d56454d98
Lucas MAGNIER <lmagnier@dawan.fr>
2023-07-04 09:49:20 +0200

    up

--- JsonEditor_form/Properties/Settings.Designer.cs
+++ JsonEditor_form/Properties/Settings.Designer.cs
@@ -10,10 +10,9 @@
 
 using JsonEditor_form.models;
 
-namespace JsonEditor_form.Properties
-{
-
-
+namespace JsonEditor_form.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
